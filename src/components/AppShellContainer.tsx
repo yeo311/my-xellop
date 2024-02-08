b03b3872b21fc55d81170b0663d6bf3eb54a0c04
Jihoon Yeo <35023979+yeo311@users.noreply.github.com>
2024-02-08 17:05:59 +0900

    AppShellContainer 컴포넌트의 푸터 높이를 환경 변수를 고려한 값으로 변경 (#9)

--- src/components/AppShellContainer.tsx
+++ src/components/AppShellContainer.tsx
@@ -6,7 +6,11 @@ import Navagation from './Navigation'
 
 export default function AppShellContainer({ children }: PropsWithChildren) {
   return (
-    <AppShell header={{ height: 60 }} footer={{ height: 60 }} padding="md">
+    <AppShell
+      header={{ height: 60 }}
+      footer={{ height: 'calc(env(safe-area-inset-bottom) + 60px)' }}
+      padding="md"
+    >
       <AppShell.Header>
         <Group h="100%" pl="15px" justify="center">
           <Title order={3}>차량 방문등록</Title>
