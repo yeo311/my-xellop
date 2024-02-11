6edd670c50f5a920e4cb56b4cc01b8712c3caafc
Jihoon Yeo <35023979+yeo311@users.noreply.github.com>
2024-02-11 22:16:15 +0900

    PWA manifest 수정 (#14)

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,27 +2,13 @@
 
 import { MantineProvider } from '@mantine/core'
 import { DatesProvider } from '@mantine/dates'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { PropsWithChildren, useState } from 'react'
 import 'dayjs/locale/ko'
 
 export default function Providers({ children }: PropsWithChildren) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000,
-          },
-        },
-      })
-  )
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <MantineProvider>
-        <DatesProvider settings={{ locale: 'ko' }}>{children}</DatesProvider>
-      </MantineProvider>
-    </QueryClientProvider>
+    <MantineProvider>
+      <DatesProvider settings={{ locale: 'ko' }}>{children}</DatesProvider>
+    </MantineProvider>
   )
 }
--- src/module/fetchApi.ts
+++
@@ -1,3 +0,0 @@
-export default function fetchApi<T>(url: string, options: RequestInit): Promise<T> {
-  return fetch(url, options).then((res) => res.json())
-}
